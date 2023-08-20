6676818a0f4b4dcf8c4fd0eb50aadd093b0ac819
abhishek <abhishekgobburkar@gmail.com>
2023-08-21 00:20:23 +0530

    initial _app

---
+++ src/component/footer/footer.jsx
@@ -0,0 +1,11 @@
+// import React from 'react'
+import './Footer.css'
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Developed by Abhishek R G</p>
+    </div>
+  );
+}
+
+export default Footer
