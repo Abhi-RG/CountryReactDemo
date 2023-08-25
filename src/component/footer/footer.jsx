a70bc3e4bf8bceb11b7b7010efe7af7fa0b100ac
abhishek <abhishekgobburkar@gmail.com>
2023-08-25 23:37:04 +0530

    Footer_Fix

--- src/component/footer/footer.jsx
+++ src/component/footer/footer.jsx
@@ -3,7 +3,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <div className="footer">
-      <p>Developed by Abhishek R G</p>
+      &copy; {new Date().getFullYear()} Developed by Abhishek{" "}
     </div>
   );
 }
